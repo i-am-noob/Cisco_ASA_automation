bdb7c53ef3d26139461c769485ab71b74ae9440e
i-am-noob <spandanshrestha4@gmail.com>
2026-01-14 16:48:09 +0000

    Change to add_user

--- add_user.py
+++ add_user.py
@@ -6,7 +6,6 @@ import ast
 
 load_dotenv()
 ip_list_srt = os.getenv("IPADDRESS_LIST")
-
 IPADDRESS = ast.literal_eval(ip_list_srt)
 GROUP_VALUE = os.getenv("GROUP_VALUE")
 GROUP_POLICY = os.getenv("GROUP_POLICY")
@@ -22,13 +21,16 @@ def add_3p_user(device,username_3p):
         "service-type remote-access",
         f"group-lock value {GROUP_VALUE}",
         f"vpn-group-policy {GROUP_POLICY}"
-        ]   
+        ]
+    try:
 
-    net_connect = ConnectHandler(**device)
-    output = net_connect.send_config_set(commands, read_timeout=180)
+        net_connect = ConnectHandler(**device)
+        output = net_connect.send_config_set(commands, read_timeout=180)
+        net_connect.save_config()
+        net_connect.disconnect()
 
-    net_connect.save_config()
-    net_connect.disconnect()
+    except:
+        print("Error connecting to device")
 
     return output
 
@@ -37,13 +39,13 @@ def add_3p_user(device,username_3p):
 def main():
     username, password = get_credentials()
     device_ips = IPADDRESS
+    username_3p = input("Enter 3P username: ")
 
     for ip in device_ips:
-        device = create_device(ip, username, password)
-        
+        device = create_device(ip, username, password)        
         # Connect to the device
         print(f"Connecting to {ip}.............................\n")
-        username_3p = input("Enter 3P username: ")
+        
         add_user = add_3p_user(device, username_3p)
         print(add_user)
 
